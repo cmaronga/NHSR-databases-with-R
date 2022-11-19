b29afd2fa7a3c81a7be39ec33605323450e2d41e
cmaronga <marongachristopher@gmail.com>
2022-11-19 09:15:59 +0000

    adding database info documentation

--- exercises/01_exercise-RDBMS-a.R
+++ exercises/01_exercise-RDBMS-a.R
@@ -4,5 +4,11 @@
 
 
 # load required packages --------------------------------------------------
-
-
+ 
+library(DBI)        # Main database interface for R
+library(dbplyr)     # Handles dplyr translation to SQL
+library(tidyverse)  # tidyverse world of package (including dplyr, ggplot2, etc.)
+library(RSQLite)    # DBI back-end package for connecting to SQLite
+library(odbc)       # DBI back-end  to connect to most RDBMS & SQL server
+library(RMySQL)     # DBI back-end package for connecting to MySQL
+library(redcapAPI)  # Connecting to REDCap database
