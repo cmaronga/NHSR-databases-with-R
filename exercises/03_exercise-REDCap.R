201b19cada1be8d5e2eb311c5f5ab4d57d8b0ac6
cmaronga <marongachristopher@gmail.com>
2022-11-20 11:55:59 +0000

    adding exercise 3

--- exercises/03_exercise-REDCap.R
+++ exercises/03_exercise-REDCap.R
@@ -5,3 +5,16 @@
 
 # load required packages --------------------------------------------------
 
+library(DBI)        # Main database interface for R
+library(dbplyr)     # Handles dplyr translation to SQL
+library(tidyverse)  # tidyverse world of package (including dplyr, ggplot2, etc.)
+library(RSQLite)    # DBI back-end package for connecting to SQLite
+library(odbc)       # DBI back-end  to connect to most RDBMS & SQL server
+library(RMySQL)     # DBI back-end package for connecting to MySQL
+library(redcapAPI)  # Connecting to REDCap database
+
+# Task : Establish a connection to REDCap database`NHS Conference` and use it to
+# complete the below task.
+
+
+#(i) Extract data from the four events (Separately) and join into a single dataset.
