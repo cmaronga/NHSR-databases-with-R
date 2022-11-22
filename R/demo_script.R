b5ab70ee5d675eb6d89f5419866114643489bff9
cmaronga <marongachristopher@gmail.com>
2022-11-22 23:36:39 +0000

    updated main slides

---
+++ R/demo_script.R
@@ -0,0 +1,4 @@
+# About this script -------------------------------------------------------
+# Author: Christopher Maronga
+# Purpose: Demonstration codes for the workshop
+# Date created: 14/10/2022
